5c21ed42bf93e8a5b599dacf08aa2aceb38523b2
yara265 <yaranuesoli@gmail.com>
2024-09-30 10:50:56 -0400

    URL E PORTA

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 //importa o mongodb em MongoCLient
 const { MongoClient, ObjectId } = require('mongodb');
 // configura a url do banco
-const url="mongodb://127.0.0.1:27017";
+const url="mongodb+srv://yaranuesoli:<0yyD09n0k3KX83vX>@cluster0.506vc.mongodb.net"
 // nome do banco de dados
 const dbName = "backend-abril-24";
 // cria um cliente com a url criada
@@ -95,7 +95,7 @@ async function main(){
     res.send("Item Removido com Sucesso");
   })
 
-  app.listen(3000);
+  app.listen(process.env.PORT||3000);
 
 }
 
